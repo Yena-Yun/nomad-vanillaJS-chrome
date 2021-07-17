7f39c859616e577a05cd144e17f46ea3ebfc58db
Yena-Yun <cheryleduart@gmail.com>
2021-07-17 15:08:47 +0900

    console과 alert 용도 차이, input 유효성 검사, form 특징

--- 2회차/js/login.js
+++ 2회차/js/login.js
@@ -13,7 +13,28 @@ function onLoginBtnClick() {
 
 	//console.log("click!");
 
-	// ** validation 작업: user가 인풋창에 뭔가(이름)를 입력했을 때만 버튼을 클릭할 수 있게 함
+	// ** validation(유효성 검사)
+	// 1) input창에는 반드시 뭔가를 입력해야 함 (필수입력항목)
+	// 2) input창의 내용이 너무 길면 안됨
+	const username = loginInput.value;
+	// if (username === "") {
+	// 	//console.log("Please write your name.");
+	// 	//콘솔창은 개발자가 보는 용도 => 유저가 볼 수 있게 하려면 alert 사용
+	// 	alert("Please write your name.");
+	// } else if (username.length > 15) {
+	// 	alert("Your name is too long!");
+	// }
+
+	// 위에서 진행한 validation을 브라우저에서 자체 제공하는 input의 속성으로 해결 가능함 => 1): required, 2): maxlength
+	//	**주의: 이러한 input 자체의 유효성 검사가 작동하려면
+	//					input이 form 태그 안에 있어야 함
+	// => div를 form 태그로 바꾸고, username을 아직 받는다는 걸 확인하기 위해 콘솔에 찍어봄
+	console.log(username); //input의 유효성 검사가 작동하기 시작 (아무 것도 입력 안 하면 경고창 뜨고, 15자 이상 입력이 안됨)
+
+	// + form의 특징
+	// 1) 버튼을 클릭하지 않고 엔터만 쳐도 submit됨
+	// 2) submit 시 브라우저가 새로고침 됨
+	//	=> 개발자의 할 일: 브라우저가 새로고침하지 않고 user 정보를 저장하게 함
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
